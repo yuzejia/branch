06b423f8a39f4a4a729889f7cd5858acdcfa48c1
yu <755596386@qq.com>
2017-05-14 11:36:18 +0800

    替换整个frinend,实现路由跳转测试

---
+++ Friend/js/app/friend.js
@@ -0,0 +1,31 @@
+var app = angular.module("myApp",[]);
+    app.controller("Attention",["$scope",function ($scope) {
+        $scope.lays = function () {
+            var Hbody = window.screen.height ;
+            $(".Friend .Fmodel").css({"height":Hbody,"display":"block"});
+            $scope.lays2 = true;
+            $(".Friend .lays").css({"bottom":0})
+        };
+        /*取消关注*/
+        // $scope.cancelAttention = function () {
+        //
+        // }；
+        /*取消底层弹框*/
+        $scope.cancel = function () {
+            $scope.lays2 = false;
+            $(".Friend .Fmodel").css("display","none");
+        }
+    }]);
+    
+app.controller("myFriend",["$scope","$location",function($scope,$location){
+	$scope.AttentionHref = function(path){
+		console.info($location);
+		var a = $location.absUrl();
+		var aa = a.lastIndexOf("/");
+		var aaa = a.substring(0,aa);
+		var aaaa = aaa+"/friendAttention.html";
+		console.log(window.location)
+//		window.location.href = aaaa;
+		
+	}
+}])
